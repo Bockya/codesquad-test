f286c1a9b940389fb795c89ed1a0310c3636af85
Bockya <rhk3562@naver.com>
2020-12-13 22:41:09 +0900

    Success step1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,46 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("> ");
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        int N = sc.nextInt();
+        String LR = sc.next();
+        String result = checkLR(word, N, LR);
+        System.out.println(result);
+    }
+
+    private static String checkLR(String word, int N, String LR) {
+        String newWord = "";
+        while (N != 0) {
+            int shift = N % word.length();
+            if (shift >= 0) {
+                if (LR.equalsIgnoreCase("L"))
+                    newWord = shiftLeft(word, shift);
+                if (LR.equalsIgnoreCase("R"))
+                    newWord = shiftRight(word, shift);
+            }
+            if (shift < 0) {
+                shift = -shift;
+                if (LR.equalsIgnoreCase("L"))
+                    newWord = shiftRight(word, shift);
+                if (LR.equalsIgnoreCase("R"))
+                    newWord = shiftLeft(word, shift);
+            }
+            N /= word.length();
+        }
+        return newWord;
+    }
+
+    private static String shiftRight(String s, int n) {
+        return shiftLeft(s, s.length() - n);
+    }
+
+    private static String shiftLeft(String s, int n) {
+        return s.substring(n) + s.substring(0, n);
     }
 }
